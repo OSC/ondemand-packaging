6079c9097feef48fd06cf7dc4b988f312405a475
Trey Dockendorf <tdockendorf@osc.edu>
2017-09-11 10:16:59 -0400

    Fix setup_sources.sh for new paths

--- setup_sources.sh
+++ setup_sources.sh
@@ -3,12 +3,15 @@
 dir='*'
 [ -n "$1" ] && dir=$1
 
-for spec in $dir/*.spec; do
-  d=$(dirname $spec)
-  for source in $(spectool --list-files $spec | awk '{print $2}'); do
-    sourcebase=$(basename "$source")
-    [ -h $d/$sourcebase ] || continue
-    git annex whereis "$d/$sourcebase" 2>/dev/null | grep -q " web:" && continue
-    git annex addurl --file "$d/$sourcebase" "$source"
+for subdir in dependencies; do
+  for spec in $subdir/$dir/*.spec; do
+    d=$(dirname $spec)
+    for source in $(spectool --list-files $spec | awk '{print $2}'); do
+      sourcebase=$(basename "$source")
+      [ -h $d/$sourcebase ] || continue
+      git annex whereis "$d/$sourcebase" 2>/dev/null | grep -q " web:" && continue
+      git annex addurl --file "$d/$sourcebase" "$source"
+    done
   done
 done
+
