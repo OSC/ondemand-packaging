f1d042e296c1ee7e8c02cce642a088d17f23a3a0
Trey Dockendorf <tdockendorf@osc.edu>
2018-02-15 07:17:55 -0500

    Add release wrapper script

---
+++ release.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+declare -A release_map=(
+    ["compute"]="compute"
+    ["web-nonscl"]="web-nonscl"
+    ["web"]="web-scl"
+    ["misc/ondemand-release"]="release"
+    ["web/mod_auth_openidc"]="web-httpd24"
+)
+
+if [ $# -gt 0 ]; then
+    packages=($@)
+else
+    read -p "Are you use you want to release ALL packages? (y/n): "
+    if [[ $REPLY =~ ^[Nn]$ ]]; then
+        exit 0
+    fi
+    packages=()
+    for c in compute web-nonscl web; do
+        for p in ${DIR}/${c}/*; do
+            packages+=("$p")
+        done
+    done
+fi
+
+for p in "${packages[@]}"; do
+    package=$(basename $p)
+    c=$(basename $(dirname $p))
+    package_key=$(echo $package | sed "s/-/_/")
+    cp="${c}/${package}"
+    if [ -n "${release_map[$cp]}" ]; then
+        release="${release_map[$cp]}"
+    else
+        release="${release_map[$c]}"
+    fi
+    echo "RELEASE: $package"
+    cd $p
+    RSYNC_USERNAME=mirror tito release --all-starting-with=$release
+    cd $DIR
+done
+
+exit 0
