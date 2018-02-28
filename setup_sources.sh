9184893f191226fb2582961febb6dd65de7e1ef7
Trey Dockendorf <tdockendorf@osc.edu>
2018-02-28 14:55:23 -0500

    Improve setup_sources.sh

--- setup_sources.sh
+++ setup_sources.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -x
 
-dir='*'
-[ -n "$1" ] && dir=$1
-
-for subdir in compute web-nonscl web; do
-  for spec in $subdir/$dir/*.spec; do
-    d=$(dirname $spec)
-    for source in $(spectool --list-files $spec | awk '{print $2}'); do
-      sourcebase=$(basename "$source")
-      [ -h $d/$sourcebase ] || continue
-      git annex whereis "$d/$sourcebase" 2>/dev/null | grep -q " web:" && continue
-      git annex addurl --file "$d/$sourcebase" "$source"
+if [ $# -gt 0 ]; then
+    dirs=($@)
+else
+    specs=()
+    for c in compute web-nonscl web; do
+        for d in ${c}/*; do
+            dirs+=("$d")
+        done
     done
-  done
-done
+fi
 
+for dir in "${dirs[@]}"; do
+    for spec in $dir/*.spec; do
+        d=$(dirname $spec)
+        for source in $(spectool --list-files $spec | awk '{print $2}'); do
+            sourcebase=$(basename "$source")
+            [ -h $d/$sourcebase ] || continue
+            git annex whereis "$d/$sourcebase" 2>/dev/null | grep -q " web:" && continue
+            git annex addurl --file "$d/$sourcebase" "$source"
+        done
+    done
+done
