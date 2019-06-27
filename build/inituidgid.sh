d925dfdfb5187cdfbdb4e652fd8d2275145375c8
Trey Dockendorf <tdockendorf@osc.edu>
2019-06-27 14:01:04 -0400

    See if non-unique UID/GID will fix issues with gitlab builds

--- build/inituidgid.sh
+++ build/inituidgid.sh
@@ -14,8 +14,8 @@ if awk -F: '{ print $3 }' < /etc/group | grep -q "^${OOD_GID}$"; then
 fi
 
 chown -R "$OOD_UID:$OOD_GID" /home/ood
-groupmod -g "$OOD_GID" ood
-usermod -u "$OOD_UID" -g "$OOD_GID" ood 2>/dev/null 1>/dev/null
+groupmod -o -g "$OOD_GID" ood
+usermod -o -u "$OOD_UID" -g "$OOD_GID" ood 2>/dev/null 1>/dev/null
 
 # There's something strange with either Docker or the kernel, so that
 # the 'app' user cannot access its home directory even after a proper
