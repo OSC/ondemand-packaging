3e9b706a5291b6831c61f4ede95875790d1db297
Trey Dockendorf <tdockendorf@osc.edu>
2021-11-10 07:03:13 -0500

    Remove unnecessary bootstrap logic

--- lib/ood_packaging/build_box/docker-image/inituidgid.sh
+++ lib/ood_packaging/build_box/docker-image/inituidgid.sh
@@ -11,13 +11,6 @@ chown -R "$OOD_UID:$OOD_GID" /home/ood
 groupmod -o -g "$OOD_GID" ood
 usermod -o -u "$OOD_UID" -g "$OOD_GID" ood 2>/dev/null 1>/dev/null
 
-# There's something strange with either Docker or the kernel, so that
-# the 'app' user cannot access its home directory even after a proper
-# chown/chmod. We work around it like this.
-mv /home/ood /home/ood2
-cp -dpR /home/ood2 /home/ood
-rm -rf /home/ood2
-
 set +e
 SCL_SOURCE="$(command -v scl_source)"
 [[ "${SCL_SOURCE}" ]] && source "${SCL_SOURCE}" enable "rh-ruby27" &> /dev/null
