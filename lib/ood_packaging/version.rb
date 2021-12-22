a4ce076501742d560f905b0200e5cf7611e10480
Trey Dockendorf <tdockendorf@osc.edu>
2021-12-22 12:08:04 -0500

    Bump RC version

--- lib/ood_packaging/version.rb
+++ lib/ood_packaging/version.rb
@@ -2,7 +2,7 @@
 
 # Version code for OodPackaging
 module OodPackaging
-  VERSION = '0.0.1.rc.5'
+  VERSION = '0.0.1.rc.6'
   PACKAGE_VERSION = {
     'ondemand-release'        => {
       '(ubuntu|debian)' => '2.1.0',
