526c949afa56cc4c32e2b487da1ea21ef8b0b29a
Trey Dockendorf <tdockendorf@osc.edu>
2022-07-19 13:53:55 -0400

    Fix ondemand-nginx package version for Ubuntu builds Show packages after build Bump version

--- lib/ood_packaging/version.rb
+++ lib/ood_packaging/version.rb
@@ -2,7 +2,7 @@
 
 # Version code for OodPackaging
 module OodPackaging
-  VERSION = '0.0.1.r2.0'
+  VERSION = '0.0.2.r2.0'
   PACKAGE_VERSION = {
     'ondemand-release'        => {
       '(ubuntu|debian)' => '2.0.0',
