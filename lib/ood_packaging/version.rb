f4528f42c36dc1c4dd60afae71acb9fd49f54f06
treydock <tdockendorf@osc.edu>
2023-10-06 16:40:19 -0400

    Avoid binfmt automount in containers that seems to be causing issues possibly (#253)

--- lib/ood_packaging/version.rb
+++ lib/ood_packaging/version.rb
@@ -2,7 +2,7 @@
 
 # Version code for OodPackaging
 module OodPackaging
-  VERSION = '0.12.0'
+  VERSION = '0.12.1'
   PACKAGE_VERSION = {
     'ondemand-release'        => {
       '(ubuntu|debian)' => '3.1.0',
