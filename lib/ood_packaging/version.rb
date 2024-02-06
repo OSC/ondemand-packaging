ecaec7cb3a01f4922849346a46551fed6d8339eb
treydock <tdockendorf@osc.edu>
2024-02-06 14:24:44 -0500

    Fix Ruby version for ondemand-runtime (#267)
    * Fix Ruby version for ondemand-runtime
    
    * Pull first
    
    * Use latest runtime with passenger RPM build
    
    * Avoid duplicate repo update

--- lib/ood_packaging/version.rb
+++ lib/ood_packaging/version.rb
@@ -12,7 +12,7 @@ module OodPackaging
       '(ubuntu|debian)' => '3',
       'default'         => '1-8'
     },
-    'ondemand-runtime'        => '3.1.4',
+    'ondemand-runtime'        => '3.1.5',
     'scl-utils'               => '2.0.3',
     'passenger'               => '6.0.17',
     'cjose'                   => '0.6.1',
