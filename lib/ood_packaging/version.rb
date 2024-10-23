f777543e6818c47063ab1c1f4287493b32b634cb
treydock <tdockendorf@osc.edu>
2024-10-23 10:26:41 -0400

    Do not put NodeJS repo into Ubuntu 24.04 build image (#281)
    * Do not put NodeJS repo into Ubuntu 24.04 build image
    Fixes #280
    
    * Future proof conditional

--- lib/ood_packaging/version.rb
+++ lib/ood_packaging/version.rb
@@ -2,7 +2,7 @@
 
 # Version code for OodPackaging
 module OodPackaging
-  VERSION = '0.15.1'
+  VERSION = '0.15.2'
   PACKAGE_VERSION = {
     'ondemand-release'        => {
       '(ubuntu|debian)' => '3.1.2',
