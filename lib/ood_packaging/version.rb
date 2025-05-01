26cba4980e4207725177b87a813a06b5f607840a
treydock <tdockendorf@osc.edu>
2025-05-01 13:53:31 -0400

    Release 0.17.1 (#302)

--- lib/ood_packaging/version.rb
+++ lib/ood_packaging/version.rb
@@ -2,7 +2,7 @@
 
 # Version code for OodPackaging
 module OodPackaging
-  VERSION = '0.16.2'
+  VERSION = '0.17.1'
   PACKAGE_VERSION = {
     'ondemand-release'        => {
       '(ubuntu|debian)' => '4.0.0',
