c60f9653a5662090f7fb086d47eab4c198dc5ce3
Trey Dockendorf <tdockendorf@osc.edu>
2026-03-30 09:46:54 -0400

    Support Debian 13

--- lib/ood_packaging/version.rb
+++ lib/ood_packaging/version.rb
@@ -2,7 +2,7 @@
 
 # Version code for OodPackaging
 module OodPackaging
-  VERSION = '0.20.0'
+  VERSION = '0.21.0'
   PACKAGE_VERSION = {
     'ondemand-release'        => {
       '(ubuntu|debian)' => '4.2.0',
